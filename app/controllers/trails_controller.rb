ad6baea5e16bb0e554dcff21a8c6bd01f9de4735
Manoj Panta <manojpanta95@gmail.com>
2018-06-01 10:35:03 -0600

    done with all the user stories

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -3,8 +3,6 @@ class TrailsController < ApplicationController
   def show
     @trail = Trail.find(params[:id])
     @total_trips = @trail.total_trips
+    @trips = @trail.trips
   end
-
-
-
 end
--- spec/features/visitor_visiting_trail_show_page_can_see_trail_details_spec.rb
+++ spec/features/visitor_visiting_trail_show_page_can_see_trail_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-describe 'visitor visiting trips index' do
-  it 'can show all the trips names' do
+describe 'visitor visiting trail show page ' do
+  it 'can show all the the trail details' do
 
     trail = Trail.create(name: 'long trail', length: 3, address: 'denver')
 
@@ -9,6 +9,27 @@ describe 'visitor visiting trips index' do
     expect(page).to have_content("This is #{trail.name}")
     expect(page).to have_content(trail.address)
     expect(page).to have_content(trail.length)
+  end
+
+  it 'can show all the trip taken and total trip distance' do
+
+    trail = Trail.create(name: 'long trail', length: 3, address: 'denver')
+    trail1 = Trail.create(name: 'long trail', length: 4, address: 'denver')
+
+    trip = trail.trips.create(name: 'trip1', start_date: '02/03/13', end_date: '02/03/13')
+
+    trip1 = trail1.trips.create(name: 'trip1', start_date: '02/03/13', end_date: '02/03/13')
+
+    trip1 = trail.trips.create(name: 'this is new trip', start_date: '02/02/13', end_date: '02/03/13')
+
+
+    visit trail_path(trail)
+
+    expect(page).to have_content(trip.name)
+    expect(page).to have_content(trip.total_hiking_distance)
+
+    expect(page).to have_content(trip1.name)
+    expect(page).to have_content(trip1.total_hiking_distance)
   end
 
   it 'can show total number of hiking trips where this trail was included' do
